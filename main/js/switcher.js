2afbebf59dc392164908c19eb0eac3dac076773f
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2016-02-19 17:17:14 +0530

    Added change_frame

--- main/js/switcher.js
+++ main/js/switcher.js
@@ -1,10 +1,18 @@
 $(function(){
 
-	$('#backgrounds img').click(function() {
-	
-		var newClass = 'BG' + $(this).attr('id');
-		$('body').removeClass().addClass(newClass);
-	
-	});
-	
+    $('#backgrounds img').click(function() {
+
+        var newClass = 'BG' + $(this).attr('id');
+        $('body').removeClass().addClass(newClass);
+
+    });
+
 });
+
+function change_frame(parent_frame,child_frame){
+    document.getElementById(parent_frame+"_content").innerHTML = document.getElementById(child_frame+"_content").innerHTML;
+    document.getElementById(parent_frame+"_tagline").innerHTML = document.getElementById(child_frame+"_tagline").innerHTML;
+    document.getElementById(parent_frame+"_header").innerHTML = document.getElementById(child_frame+"_header").innerHTML;
+    document.getElementById(parent_frame+"_image").innerHTML = document.getElementById(child_frame+"_image").innerHTML;
+}
+
